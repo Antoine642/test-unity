36043bbed1ad31c184c8850543de3396c7b67953
Antoine Park <asuse35@hotmail.fr>
2025-01-27 16:41:14 +0100

    auto shoot rupee

--- Assets/Scrtipts/RupeeManager.cs
+++ Assets/Scrtipts/RupeeManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,31 @@ public class RupeeManager : MonoBehaviour
     public Transform spawner;
     public Rupee prefab;
     public Transform container;
+    [Range(2, 10)]
     public float spawnDelay = 2f;
     private readonly List<Rupee> _rupees = new List<Rupee>();
     private Coroutine _spawnRoutine;
-    
+
+    public void Start()
+    {
+        StartSpawning();
+    }
+
+    public void StartSpawning()
+    {
+        _spawnRoutine = StartCoroutine(SpawnRoutine());
+    }
+
+    private void Spawn()
+    {
+        var rupee = Instantiate(prefab, spawner.position, Quaternion.identity);
+        rupee.transform.parent = container;
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        Spawn();
+        yield return new WaitForSeconds(spawnDelay);
+        StartSpawning();
+    }
 }
